9375ecb7244be0c51b0488fd6a5bb4e725258b68
Vikram Bhatt <33462780+vikrambhatt1405@users.noreply.github.com>
2018-12-10 20:25:46 +0530

    Add files via upload

---
+++ tf_graph_functions.py
@@ -0,0 +1,85 @@
+def assign_global_to_local(global_to_local):
+	"""Assigns global variable value to local variables.
+
+	global_to_local : dictionary with corresponding local variable for global key
+	"""
+	r = []
+	for v in global_to_local.keys():
+		r.append(tf.assign(global_to_local[v],v))
+	with tf.control_dependencies(r):
+		a = tf.no_op()
+	return a
+
+
+def assign_local_to_global(local_to_global):
+	"""Assigns global variable value to local variables.
+
+	local_to_global : dictionary with corresponding global variable for local key
+	"""
+	r= []
+	for v in local_to_global.keys():
+		r.append(tf.assign(local_to_global[v],v))
+	with tf.control_dependencies(r):
+		a = tf.no_op()
+	return a
+
+
+def get_variable_by_name(name):
+	"""Returns the variable of given name
+
+	name : the name of the global variable
+	"""
+	return [v for v in tf.get_collection('variables') if v.name == name][0]
+
+
+def get_global_variable_by_name(name):
+	"""Returns the global variable of given name.
+
+	name : the name of the global variable
+	"""
+	# return [v for v in tf.variables() if v.name == name][0]
+	return [v for v in tf.global_variables() if v.name == name][0]
+
+
+def create_global_variables(local_optimizer_vars = []):
+	"""Creates global variables for local variables on the graph.
+	Skips variables local variables that are created for
+	local optimization.
+
+	Returns dictionarys for local-to-global and global-to-local
+	variable mappings.
+	"""
+	local_to_global = {}
+	global_to_local = {}
+	with tf.device('/job:ps/task:0'):
+		for v in tf.local_variables():
+			if v not in local_optimizer_vars:
+				v_g = tf.get_variable('g/'+v.op.name,
+					shape = v.shape,
+					dtype = v.dtype,
+					trainable=True,
+					collections=[tf.GraphKeys.GLOBAL_VARIABLES,
+								tf.GraphKeys.TRAINABLE_VARIABLES])
+				local_to_global[v] = v_g
+				global_to_local[v_g] = v
+	return local_to_global,global_to_local
+
+
+def add_global_variables_to_local_collection():
+	"""Adds all variables from the global collection
+	to the local collection.
+
+	Returns the list of variables added.
+	"""
+	r =[]
+	for var in tf.get_default_graph()._collections[tf.GraphKeys.GLOBAL_VARIABLES]:
+		tf.add_to_collection(tf.GraphKeys.LOCAL_VARIABLES,var)
+		r.append(var)
+	return r
+
+
+def clear_global_collection():
+	"""Removes all variables from global collection."""
+	g = tf.get_default_graph()
+	for _ in range(len(g._collections[tf.GraphKeys.GLOBAL_VARIABLES])):
+		del g._collections[tf.GraphKeys.GLOBAL_VARIABLES][0]
